aafc83553fa75b58a12f856db27374077cd5ff66
Haojie Liu <haojie.liu.stud@ispascalcomandini.it>
2023-06-01 16:01:58 +0200

    Controller

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
+import { Dvd } from "./model/Dvd";
+import { IGetInfo } from "./model/IGetInfo";
 import { Libro } from "./model/Libro";
 import { LibroCartaceo } from "./model/LibroCartaceo";
 import { LibroDigitale } from "./model/LibroDigitale";
 
-let libreria : Libro[] = [];
+let libreria : IGetInfo[] = [];
 
 libreria.push(new LibroCartaceo('I Promessi Sposi','Alessandro Manzoni',400));
 libreria.push(new LibroDigitale('La Divina Commedia','Dante Alighieri',500));
+libreria.push(new Dvd('Guerre stellari','Non lo ricordo'));
+
 libreria.forEach(l => console.log(l.getInfo()));
