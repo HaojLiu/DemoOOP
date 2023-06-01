014b1fd005d0bb91dc67aa46cd2063de25b0c09b
Haojie Liu <haojie.liu.stud@ispascalcomandini.it>
2023-06-01 14:39:09 +0200

    Export Classes

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-let l = new Libro("I promessi sposi","Alessandro Manzoni");
+import { Libro } from "./model/Libro";
+import { LibroCartaceo } from "./model/LibroCartaceo";
+import { LibroDigitale } from "./model/LibroDigitale";
+
+let libreria : Libro[] = [];
+
+libreria.push(new LibroCartaceo('I Promessi Sposi','Alessandro Manzoni',400));
+libreria.push(new LibroDigitale('La Divina Commedia','Dante Alighieri',500));
+libreria.forEach(l => console.log(l.getInfo()));
